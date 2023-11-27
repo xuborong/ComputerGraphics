59f35b38eb365f27164eec1067aaebbfdcb08410
xuborong <alexxu@wustl.edu>
2023-11-27 17:58:03 -0500

    Bug fixes; prototype works

--- model.py
+++ model.py
@@ -1,19 +1,54 @@
+import numpy as np
+from pathlib import Path
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 from sklearn.model_selection import train_test_split
 from time_synchronize import synchronize
 
-X_train, X_val, X_test = ...
-y_train, y_val, y_test = ...
+# Define directories
+face_directory = Path("sneezes/face_parameters")
+audio_directory = Path("sneezes/sneeze_audio")
 
-model = Sequential()
-model.add(LSTM(50, return_sequences=True, input_shape=(X_train.shape[1], X_train.shape[2])))
-model.add(LSTM(50))
-model.add(Dense(y_train.shape[1]))  # Output layer with a unit for each facial parameter
+# Gather file paths
+audio_paths = [str(p).replace('\\', '/') for p in audio_directory.rglob('*') if p.is_file()]
+face_paths = [str(p).replace('\\', '/') for p in face_directory.rglob('*') if p.is_file()]
 
+# Initialize data arrays
+audio_data, face_data = [], []
+
+# Synchronize audio and face data
+for audio_path, face_path in zip(audio_paths, face_paths):
+    face, audio = synchronize(audio_path, face_path)
+    audio_data.append(audio)
+    face_data.append(face)
+
+# Find the maximum length of sequences in audio_data and face_data
+max_audio_length = max(array.shape[0] for array in audio_data)
+max_face_length = max(array.shape[0] for array in face_data)
+
+# Pad audio_data and face_data to the maximum length
+audio_data_padded = np.array([np.pad(item, ((0, max_audio_length - item.shape[0]), (0, 0)), 'constant') for item in audio_data])
+face_data_padded = np.array([np.pad(item, ((0, max_face_length - item.shape[0]), (0, 0)), 'constant') for item in face_data])
+
+# Reshape the target data (face_data) to match the output shape of the model
+# Assuming each facial data sample should be a flat array
+face_data_flattened = face_data_padded.reshape(face_data_padded.shape[0], -1)
+
+# Split data into train, validation, and test sets
+X_temp, X_test, y_temp, y_test = train_test_split(audio_data_padded, face_data_flattened, test_size=0.15, random_state=42)
+X_train, X_val, y_train, y_val = train_test_split(X_temp, y_temp, test_size=0.18, random_state=42)
+
+# Model architecture
+model = Sequential([
+    LSTM(50, return_sequences=True, input_shape=(X_train.shape[1], X_train.shape[2])),
+    LSTM(50),
+    Dense(y_train.shape[1])  # Adjusted to match the flattened facial data
+])
+
+# Compile and train the model
 model.compile(optimizer='adam', loss='mean_squared_error')
-
 model.fit(X_train, y_train, epochs=100, validation_data=(X_val, y_val))
 
+# Evaluate the model
 test_loss = model.evaluate(X_test, y_test)
 print(f"Test Loss: {test_loss}")
--- time_synchronize.py
+++ time_synchronize.py
@@ -3,43 +3,58 @@ import librosa
 import librosa.feature as lf
 import numpy as np
 
-
 def synchronize(audio_file, face_file):
+    # Load audio
     y, sr = librosa.load(audio_file, sr=None)
-    facial_data = pd.read_csv(face_file).drop("BlendshapeCount", axis=1)
-    audio_data = pd.DataFrame([])
 
-    def timecode_to_seconds(timecode):
-        hours, minutes, seconds, milli_fraction = timecode.split(':')
-        milliseconds, fraction = milli_fraction.split('.')
-        total_seconds = (int(hours) * 3600 +
-                         int(minutes) * 60 +
-                         int(seconds) +
-                         int(milliseconds) / 1000 +
-                         int(fraction) / 1000000)
-        return total_seconds
+    # Load and preprocess facial data
+    facial_data = pd.read_csv(face_file)
+    facial_data['Time_in_Seconds'] = facial_data['Timecode'].apply(timecode_to_seconds)
+    start_time_seconds = facial_data['Time_in_Seconds'].iloc[0]
+    facial_data['Time_Offset'] = facial_data['Time_in_Seconds'] - start_time_seconds
 
-    def extract_mfcc_for_frame(frame_number, window_size=0.1):
-        start_time = max(frame_number - window_size / 2, 0)
-        end_time = start_time + window_size
-        start_sample = int(start_time * sr)
-        end_sample = int(end_time * sr)
+    # Drop non-feature columns
+    facial_data = facial_data.drop(["Timecode", "Time_in_Seconds", "BlendshapeCount"], axis=1)
 
-        segment = y[start_sample:end_sample]
-        segment_mfccs = lf.mfcc(y=segment, sr=sr, n_mfcc=13)
-        avg_mfccs = np.mean(segment_mfccs, axis=1)
-        return avg_mfccs
+    # Initialize audio feature array
+    mfccs = []
 
-    time_in_seconds = facial_data['Timecode'].apply(timecode_to_seconds)
-    start_time_seconds = time_in_seconds.iloc[0]
-    time_offset = time_in_seconds - start_time_seconds
-    audio_frames = time_offset * sr
-    audio_data["MFCC"] = audio_frames.apply(lambda frame: extract_mfcc_for_frame(frame / sr))
-    facial_data = facial_data.drop("Timecode", axis=1)
+    # Extract MFCC for each frame in facial data
+    for offset in facial_data['Time_Offset']:
+        frame_number = int(offset * sr)
+        mfcc = extract_mfcc_for_frame(y, sr, frame_number)
+        mfccs.append(mfcc)
 
-    return facial_data, audio_data
+    # Convert list of MFCCs to a consistent 2D numpy array
+    mfccs_array = np.vstack(mfccs)
 
+    return facial_data, mfccs_array
+
+def timecode_to_seconds(timecode):
+    hours, minutes, seconds, milli_fraction = timecode.split(':')
+    milliseconds, fraction = milli_fraction.split('.')
+    return int(hours) * 3600 + int(minutes) * 60 + int(seconds) + int(milliseconds) / 1000 + int(fraction) / 1000000
+
+def extract_mfcc_for_frame(audio, sr, frame_number, window_size=0.1, n_mfcc=13, default_n_fft=2048):
+    start_time = max(frame_number - window_size / 2, 0)
+    end_time = start_time + window_size
+    start_sample = int(start_time * sr)
+    end_sample = int(end_time * sr)
+
+    segment = audio[start_sample:end_sample]
+
+    # Handle zero-length segment
+    if len(segment) == 0:
+        return np.zeros(n_mfcc)
+
+    # Dynamically adjust n_fft and extract MFCCs
+    n_fft = min(len(segment), default_n_fft)
+    segment_mfccs = lf.mfcc(y=segment, sr=sr, n_mfcc=n_mfcc, n_fft=n_fft)
+    avg_mfccs = np.mean(segment_mfccs, axis=1)
+    return avg_mfccs
 
 audio_path = "sneezes/sneeze_audio/sneeze_1.wav"
 face_path = "sneezes/face_parameters/sneeze_1.csv"
 face, audio = synchronize(audio_path, face_path)
+print("Facial Data Shape:", face.shape)
+print("Audio Data Shape:", audio.shape)
